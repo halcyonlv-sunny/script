11c880644ce2158532969d62f6c305e7240cc493
Deependra Shrestha <36838592+dpenra@users.noreply.github.com>
2024-12-10 08:22:20 +0545

    Update sunny.sh

--- sunny.sh
+++ sunny.sh
@@ -43,7 +43,11 @@ git clone https://github.com/StatiXOS/android_prebuilts_gcc_linux-x86_aarch64_aa
 git clone https://github.com/StatiXOS/android_prebuilts_gcc_linux-x86_arm_arm-eabi.git --depth 1 -b 12.0.0 prebuilts/gcc/linux-x86/arm/arm-eabi
 
 # Source modifications and RisingOS-specific keys
-git clone https://github.com/rising-source-mod/android_vendor_lineage.git --depth 1 -b fifteen vendor/lineage
+# git clone https://github.com/rising-source-mod/android_vendor_lineage.git --depth 1 -b fifteen vendor/lineage
+cd vendor/lineage
+curl -s https://github.com/RisingTechOSS/android_vendor_lineage/commit/114e7aa0798e4f7bc0f8866c9979f9d5a2c72a66.patch | git am
+curl -s https://github.com/RisingTechOSS/android_vendor_lineage/commit/f8a153ad0138477c9cc2c57af95845d7d26ff1c3.patch | git am
+cd ../..
 
 . build/envsetup.sh
 riseup sunny user
